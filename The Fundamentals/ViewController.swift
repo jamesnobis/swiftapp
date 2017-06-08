e60aba53580295d6ff1d7a952aa585375fc1c3b7
James Nobis <nobiscotia@jamess-macbook-pro.local>
2017-06-08 14:24:52 +0900

    Initial Commit

---
+++ The Fundamentals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Fundamentals
+//
+//  Created by James Nobis on 2017/06/08.
+//  Copyright © 2017 James Nobis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
